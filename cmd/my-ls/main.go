33b8d054ca2596742ab42e0d710542f575e66ef2
davodhiambo <jesseomolo@yahoo.com>
2024-10-18 18:12:39 +0300

    feat: capture commandline arguments, deliver appropriate args to RetrieveFiles function, print output.

--- cmd/my-ls/main.go
+++ cmd/my-ls/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	internal "my-ls/internal/ls"
+	"os"
+)
+
 func main() {
-	
+	args := os.Args[1:]
+
+	if len(args) == 0 {
+
+	} else {
+		path := os.Args[1]
+	}
+
+	files := internal.RetrieveFile(path)
+	fmt.Println(files)
 }
