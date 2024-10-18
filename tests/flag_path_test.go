dc44ab282b8122d6dca7f60b2ae02b6faf429976
davodhiambo <jesseomolo@yahoo.com>
2024-10-18 20:02:15 +0300

    testing: add TestIsValidPath_NonEmptyString.

--- tests/flag_path_test.go
+++ tests/flag_path_test.go
@@ -1,9 +1,14 @@
 package internal_test
 
 import (
+	internal "my-ls/internal/ls"
 	"testing"
 )
 
-func TestIsValidPath(t *testing.T) {
-
+// Test non-empty paths
+func TestIsValidPath_NonEmptyString(t *testing.T) {
+	result := internal.IsValidPath("some/path")
+	if !result {
+		t.Errorf("Expected IsValidPath to return true for a non-empty string, but got false")
+	}
 }
