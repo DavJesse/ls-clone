3e9050597f5f4bc45ac639d0a0571dd7e0d0c1d4
davodhiambo <jesseomolo@yahoo.com>
2024-11-04 17:24:07 +0300

    refactor: change inputs and process of UravelFiles to access files within RecursiveList.

--- internal/ls/display.go
+++ internal/ls/display.go
@@ -3,14 +3,31 @@
 // handling file permissions, user, group, size, modification time, etc.
 package internal
 
-import "fmt"
+import (
+	"strings"
+)
 
-func UnravelFiles(files []FileInfo) {
-	//var relPath string
-	for i := range files {
-		fmt.Println(files[i].DocName)
-		if len(files[i].RecursiveList) > 0 {
-			UnravelFiles(files[i].RecursiveList)
+func UnravelFiles(dirName, indent string, files []FileInfo) string {
+	var result strings.Builder
+	result.WriteString(dirName + ":\n")
+
+	for i, file := range files {
+		result.WriteString(indent)
+		result.WriteString(file.DocName)
+
+		if i < len(files)-1 {
+			result.WriteString("\n")
+		}
+		if files[i].RecursiveList != nil || len(files[i].RecursiveList) > 0 {
+
+			result.WriteString("\n")
+			subResult := UnravelFiles(dirName+file.DocName, indent, file.RecursiveList)
+			result.WriteString(subResult)
+
+			if i < len(files)-1 {
+				result.WriteString("\n")
+			}
 		}
 	}
+	return result.String()
 }
