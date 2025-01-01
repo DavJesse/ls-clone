5c27c4eb8b9ebb453b0b220036ea0557122a047a
davodhiambo <jesseomolo@yahoo.com>
2025-01-02 01:44:41 +0300

    chore: remove unused imports.

--- internal/ls/display.go
+++ internal/ls/display.go
@@ -5,7 +5,6 @@ package internal
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"strconv"
 	"strings"
@@ -78,7 +77,7 @@ func GetMaxLength(files []FileInfo) int {
 func PrintShortList(files []FileInfo, termWidth int) {
 	// Establish working parameters
 	maxLength := GetMaxLength(files)
-	//termWidth := GetTerminalWidth()
+	// termWidth := GetTerminalWidth()
 
 	// Determine the number of columns to print to terminal
 	colums := termWidth / (maxLength + 2)
